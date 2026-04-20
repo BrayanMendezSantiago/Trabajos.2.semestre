2469ea7df0822de8ad7e10fb687193181be824ca
Brayan Mendez <bm324860@gmail.com>
2026-04-19 22:04:14 -0600

    Update Trabajo.Apuntadores.c

--- Trabajo.Apuntadores.c
+++ Trabajo.Apuntadores.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
+#include<stdlib.h>
 
 struct Dato{
   int d;
   struct Dato *ptrsig;
 };
 
+int menu(void){
+  int op;
+  printf("***Menu***");
+  printf("[1]...");
+  printf("[2]...");
+  printf("[3]...");
+  printf("[4]...");
+  scanf("%d",&op);
+return op;
+}
+
+struct Dato *dato(){
+  struct Dato *NuvD;
+  NuvD=
+  
+
 int main(void){
   struct Dato *ptr=NULL;
+  int op;
+ 
+  do{
+    op=menu();
+    switch(op){
+      case 1
 
+      break;
+    }
+  }while()
+    
 return 0;
 }
